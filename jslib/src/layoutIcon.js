711dad0bff52f1001c6a499fe5e33f646df6d09e
zhuxp <nimezhu@gmail.com>
2018-06-07 15:41:34 -0400

    improve layout icon

--- jslib/src/layoutIcon.js
+++ jslib/src/layoutIcon.js
@@ -13,15 +13,13 @@ export default function () {
     }
   }
   var layout = function (d, el) {
-    console.log("layout d?",d)
-    wh(d)
+    //wh(d)
     d.content.forEach(function (d) {
       r[d.type](d, 0, 0, 100, 100, el)
     })
   }
   var row = function (d, x, y, w, h, el) {
-    console.log("row", x, y, w, h, el)
-    wh(d)
+    //wh(d)
     if (d.content) {
       var offset = x;
       d.content.forEach(function (d) {
@@ -32,8 +30,8 @@ export default function () {
     }
   }
   var column = function (d, x, y, w, h, el) {
-    console.log("column", x, y)
-    wh(d)
+    //console.log("column", x, y)
+    //wh(d)
     if (d.content) {
       var offset = y
       d.content.forEach(function (d) {
@@ -43,25 +41,35 @@ export default function () {
     }
   }
   var stack = function (d, x, y, w, h, el) {
-    console.log("stack", x, y)
-    wh(d)
+    //console.log("stack", x, y)
+    //wh(d)
     if (d.content) {
       d.content.forEach(function (d) {
-        r[d.type](d, x+1, y+1, w-2, h-2, el) //stack Not change
+        r[d.type](d, x + 1, y + 1, w - 2, h - 2, el) //stack Not change
       })
     }
   }
   var component = function (d, x, y, w, h, el) {
-    console.log("component", x, y, w, h, d.title)
-    console.log(el)
-    el.append("rect")
+    var rect = el.append("rect")
       .attr("x", x * xscale)
       .attr("y", y * yscale)
       .attr("width", w * xscale)
       .attr("height", h * yscale)
       .attr("fill", color[d.title] || "grey")
       .attr("opacity", 0.5)
-    wh(d)
+      .on("mouseover", function () {
+        d3.select(this).attr("opacity", 0.8)
+      })
+      .on("mouseout", function () {
+        d3.select(this).attr("opacity", 0.5)
+      })
+    if (d.componentState.genome) {
+      el.append("text").attr("x", x * xscale + 5).attr("y", y * yscale + h * yscale / 2).text(d.componentState.genome)
+    }
+    rect.append("svg:title")
+      .text(d.title || "unknown")
+    //wh(d)
+
   }
 
   var r = {
