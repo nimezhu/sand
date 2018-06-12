0a87d481f7ea9a7b9c357b33c655c0a3aaf60a43
zhuxp <nimezhu@gmail.com>
2018-06-12 15:34:13 -0400

    mv cnbData

--- cnbData/data.go
+++
@@ -1,8 +0,0 @@
-package main
-
-type DataIndex struct {
-	Genome string      `json:"genome"`
-	Dbname string      `json:"dbname"`
-	Data   interface{} `json:"data"` // map[string]string or map[string][]string? could be uri or more sofisticated data structure such as binindex image
-	Format string      `json:"format"`
-}
